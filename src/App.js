a0f1a5cdbfd6f48d28b744edf5c4d6a26abb7658
Yogesh-10 <yogiyogesh100800@gmail.com>
2022-02-15 20:56:12 +0530

    battle results added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import { ThemeProvider } from './contexts/theme';
+import Nav from './components/Nav';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loading from './components/Loading';
+
+const Popular = React.lazy(() => import('./components/Popular'));
+const Battle = React.lazy(() => import('./components/Battle'));
+const Results = React.lazy(() => import('./components/Results'));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [theme, setTheme] = React.useState('light');
+	const toggleTheme = () =>
+		setTheme((theme) => (theme === 'light' ? 'dark' : 'light'));
+
+	return (
+		<Router>
+			<ThemeProvider value={theme}>
+				<div className={theme}>
+					<div className='container'>
+						<Nav toggleTheme={toggleTheme} />
+
+						<React.Suspense fallback={<Loading />}>
+							<Switch>
+								<Route exact path='/' component={Popular} />
+								<Route exact path='/battle' component={Battle} />
+								<Route path='/battle/results' component={Results} />
+								<Route render={() => <h1>404</h1>} />
+							</Switch>
+						</React.Suspense>
+					</div>
+				</div>
+			</ThemeProvider>
+		</Router>
+	);
 }
 
 export default App;
